7c1fc3adf073e4bbac815e17ad48247a45e78c60
Marcos <you@example.com>
2022-12-04 22:16:18 +0000

    Security saving

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { TouchableOpacity, StyleSheet, View, Text } from 'react-native';
 import { useEffect, useState } from "react";
-import { Dropdown } from 'react-native-element-dropdown';
 
 // importing Maps
 import MapView, { Callout, Marker } from 'react-native-maps';
@@ -17,37 +16,9 @@ const INITIAL_POSITION = {
     longitudeDelta: 5.0921,
   };
 
-// Dropdown options
-const data = [
-    { label: 'Item 1', value: '1' },
-    { label: 'Item 2', value: '2' },
-    { label: 'Item 3', value: '3' },
-    { label: 'Item 4', value: '4' },
-    { label: 'Item 5', value: '5' },
-    { label: 'Item 6', value: '6' },
-    { label: 'Item 7', value: '7' },
-    { label: 'Item 8', value: '8' },
-  ];
-
 // Home Screen
 export default function HomeScreen({ navigation }) {
 
-    // State for the dropdown
-    const [value, setValue] = useState(null);
-    const [isFocus, setIsFocus] = useState(false);
-
-    const renderLabel = () => {
-        if (value || isFocus) {
-          return (
-            <Text style={[styles.label, isFocus && { color: 'blue' }]}>
-              Dropdown label
-            </Text>
-          );
-        }
-        return null;
-    };
-
-    
     // Marker position and Map position
     const [data1, setPlaceMarkers] = useState([]);
     const [data2, setPlaceTypes] = useState([]);
@@ -117,39 +88,6 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },    
-    dropdown: {
-        height: 50,
-        borderColor: 'gray',
-        borderWidth: 0.5,
-        borderRadius: 8,
-        paddingHorizontal: 8,
-    },
-    icon: {
-        marginRight: 5,
-    },
-    label: {
-        position: 'absolute',
-        backgroundColor: 'white',
-        left: 22,
-        top: 8,
-        zIndex: 999,
-        paddingHorizontal: 8,
-        fontSize: 14,
-    },
-    placeholderStyle: {
-        fontSize: 16,
-    },
-    selectedTextStyle: {
-        fontSize: 16,
-    },
-    iconStyle: {
-        width: 20,
-        height: 20,
-    },
-    inputSearchStyle: {
-        height: 40,
-        fontSize: 16,
-    },
     placeName: {
         fontSize: 17,
         fontWeight: 'bold',
